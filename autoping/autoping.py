6d2903b04e7e3bb6c5926c9c97481fb601ac1112
Connor <connrsnow@gmail.com>
2021-05-03 08:13:09 -0700

    feat: add new subjects

--- autoping/autoping.py
+++ autoping/autoping.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 class AutoPing(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
-    self.ids = [561906912690438175, 801237878763290624, 330915985886543873]
+    self.ids = [700915834020954183, 592530158284439595, 410165721851756554]
 
   @commands.Cog.listener()
   async def on_message(self, message):
@@ -19,17 +19,17 @@ class AutoPing(commands.Cog):
         subject = ""
         link = ""
 
-        if mention.id == 561906912690438175: # Kristen, Physics Mech
-          subject = "physics"
-          link = "https://5able.me/physics-mech-cram-finale"
+        if mention.id == 700915834020954183: # JP, Calc AB
+          subject = "Calculus AB"
+          link = "https://5able.me/calc-ab-finale"
 
-        elif mention.id == 801237878763290624: # Amrita, Gov
-          subject = "government"
-          link = "https://5able.me/us-gov-cram-finale"
+        elif mention.id == 592530158284439595: # Pooja, Calc BC
+          subject = "Calculus BC"
+          link = "https://5able.me/calc-bc-finale"
 
-        elif mention.id == 330915985886543873: # Dani, Physics E & M
-          subject = "physics"
-          link = "https://5able.me/physics-cem-cram-finale"
+        elif mention.id == 410165721851756554: # Harrison APHuG
+          subject = "APHuG"
+          link = "https://5able.me/hug-cram-finale"
 
         embed = discord.Embed(
           title=":wave: Need some extra support?",

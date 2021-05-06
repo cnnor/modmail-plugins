8c3c7c015215a61456d42e09633c7a83adc25197
Connor <connrsnow@gmail.com>
2021-05-05 18:10:04 -0700

    feat: cracker bargel

---
+++ cracker-bargel/cracker-bargel.py
@@ -0,0 +1,23 @@
+import random
+
+import discord
+from discord.ext import commands
+
+from core import checks
+from core.models import PermissionLevel, getLogger
+
+logger = getLogger(__name__)
+
+class CrackerBargel(commands.Cog):
+
+  def __init__(self, bot):
+    self.bot = bot
+    self.messages = ["order corn", "cracker bargel", "BLUE CHEESE HAS MOLD IN IT", "Please Unban Me From The Group Phyllis, I Am A New Man", "martha died LOL", "I'VE REPORTED IT AS FRAUD", "It's for a church honey! NEXT!", "Your grandbabies are adorable, Agnes!", "My lake friends will understand this significance."]
+
+  @commands.Cog.listener()
+  async def on_message(self, message):
+    if "order corn" in message.content:
+      await message.channel.send(random.choice(self.messages))
+
+def setup(bot):
+  bot.add_cog(Welcomer(bot))

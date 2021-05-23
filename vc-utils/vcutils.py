bfef69abb18ad4a26f269d0e1de8de2bbbfdeadd
Zaid "Nico <me@zaid-arshad.tech>
2021-05-22 21:44:07 -0400

    fix: correctly allow targetting

--- vc-utils/vcutils.py
+++ vc-utils/vcutils.py
@@ -17,9 +17,13 @@ class VCUtils(commands.Cog):
       return
     
     if "f!forcemute" in message.content:
-      await message.author.edit(mute=True)
+      if message.mentions == []:
+        return message.channel.send("Sorry, but you have to mention someone for me to mute them!");
+      await message.mentions[0].edit(mute=True)
     if "f!unmute" in message.content:
-      await message.author.edit(mute=True)
+      if message.mentions == []:
+        return message.channel.send("Sorry, but you have to mention someone for me to mute them!");
+      await message.mentions[0].edit(mute=True)
 
 def setup(bot):
   bot.add_cog(VCUtils(bot))
